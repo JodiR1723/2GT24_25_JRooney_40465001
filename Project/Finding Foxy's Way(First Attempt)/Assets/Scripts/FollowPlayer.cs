d7346f43b6f6188afaf30d618ebcd75db8a87789
JodiR1723 <jodi.rooney@hotmail.com>
2024-11-28 20:33:34 +0000

    Testing playability

--- Project/Finding Foxy's Way(First Attempt)/Assets/Scripts/FollowPlayer.cs
+++ Project/Finding Foxy's Way(First Attempt)/Assets/Scripts/FollowPlayer.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -5.5f);
+    public Transform player;
+    public float mouseSensitivity = 2f;
+    float cameraVerticalRotation = 0f;
+
+    bool lockedCursor = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Lock and hide the cursor
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset; 
+        // Gathering mouse input
+
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        //Rotating the camera around its local X axis
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        //Rotating the camera around its Y axis
+
+        player.Rotate(Vector3.up * inputX);
     }
 }
