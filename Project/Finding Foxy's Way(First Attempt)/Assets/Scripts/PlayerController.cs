18c2ee94caa60ce77f7f6b00640885a8abf8e202
JodiR1723 <jodi.rooney@hotmail.com>
2024-12-01 19:42:19 +0000

    Adding jump and code to objects
    Allowing the player to jump and stop from falling over and adding some code to my 'powerups'

--- Project/Finding Foxy's Way(First Attempt)/Assets/Scripts/PlayerController.cs
+++ Project/Finding Foxy's Way(First Attempt)/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,18 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 80.0f;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool hasPowerup;
 
 
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    
     }
 
     // Update is called once per frame
@@ -27,5 +34,23 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Turning the player
         transform.Rotate(Vector3.up* Time.deltaTime * turnSpeed * horizontalInput);
+        //Making the player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
